7c6ad17fae821f9544af2c40765ec1a1ee921ba3
Alex <alex@MacBook-Pro-Alex.local>
2020-12-17 12:29:02 +0300

    Updated Webpack config, added tweak to component, added link to html, created new bundle

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import ExpenseListItem from "./ExpenseListItem";
-import selectExpenses from '../selectors/expenses'
+import selectExpenses from "../selectors/expenses";
 
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    {props.expenses.map((expense) => {
-      return <ExpenseListItem key={expense.id} {...expense} />;
-    })}
+    {props.expenses.length === 0
+      ? "No expenses"
+      : props.expenses.map((expense) => {
+          return <ExpenseListItem key={expense.id} {...expense} />;
+        })}
   </div>
 );
 
 const mapStateToProps = (state) => {
   return {
-    expenses: selectExpenses(state.expenses, state.filters)
+    expenses: selectExpenses(state.expenses, state.filters),
   };
 };
 
