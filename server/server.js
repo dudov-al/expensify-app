913ff2ada7283140004bd95468cebfff4d826767
Alex <alex@MacBook-Pro-Alex.local>
2020-12-21 10:19:05 +0300

    Setup production build

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ const publicPath = path.join(__dirname, "..", "public");
 
 app.use(express.static(publicPath));
 
+const port = process.env.PORT || 3000
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is up");
 });
